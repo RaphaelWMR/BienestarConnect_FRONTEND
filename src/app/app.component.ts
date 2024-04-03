defbdb1adc3ec48f64db6490a6f287f68a65edf3
RaphaelMR <matosramosraphael@gmail.com>
2024-04-03 15:23:58 -0500

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { ListAlumnosComponent } from "./components/list-alumnos/list-alumnos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    NavbarComponent,
+    ListAlumnosComponent
+  ]
 })
 export class AppComponent {
   title = 'FRONTEND';
---
+++ src/app/components/add-edit-alumno/add-edit-alumno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-alumno',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-edit-alumno.component.html',
+  styleUrl: './add-edit-alumno.component.css'
+})
+export class AddEditAlumnoComponent {
+
+}
