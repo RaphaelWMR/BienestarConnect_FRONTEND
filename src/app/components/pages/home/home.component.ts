544bdd8cea29018495bde74160834fcecd96cdad
Raphael <matosramosraphael@gmail.com>
2024-04-24 12:39:28 -0500

    alumno service

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CitaStatsService } from '../../../services/cita/cita-stats.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  cantidadCitas: number = 0;
+  porcentajeConfirmadas: number = 0;
+  ngOnInit(): void {
+    this.getCantidadCitas();
+    this.getCantidadAlumnos();
+    this.getCantidadAlumnosObs();
+    this.getPorcentajeCitasConfirmadas();
 
+  }
+  constructor(private _citaStats: CitaStatsService,) { }
+
+  getCantidadAlumnos() {
+
+  }
+
+  getCantidadAlumnosObs() {
+
+  }
+
+  getPorcentajeCitasConfirmadas() {
+    this._citaStats.getCitasConfPorc().subscribe((data: any) => {
+      this.porcentajeConfirmadas = data.porcentaje;
+    });
+  }
+
+  getCantidadCitas() {
+    this._citaStats.getCantidadCitas().subscribe((data: any) => {
+      this.cantidadCitas = data.ctasCount;
+    });
+  }
 }
