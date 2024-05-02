271c4ca012d6548c1d1ef3fb89e473111f585d5d
Raphael <matosramosraphael@gmail.com>
2024-05-02 11:44:20 -0500

    chatbot falta correcciones en el front

--- src/app/shared/pages/chatbot/chatbot-component/chatbot-component.component.ts
+++ src/app/shared/pages/chatbot/chatbot-component/chatbot-component.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { ChatbotService } from '../../../apis/service-chatbot/chatbot.service';
 
+interface Message {
+  content: string;
+  sender: 'user' | 'bot'; // Indicates who sent the message
+}
 @Component({
   selector: 'app-chatbot-component',
   templateUrl: './chatbot-component.component.html',
   styleUrl: './chatbot-component.component.css'
 })
 export class ChatbotComponentComponent {
+  chatMessages: Message[] = [];
+  userInput: string = '';
 
+  constructor(public _chatService: ChatbotService) {
+
+
+  }
+  sendMessage(userInput: string) {
+    if (userInput.trim()) {
+      // Add user message to chatMessages
+      this.chatMessages.push({ content: userInput, sender: 'user' });
+
+      // Call chatbot service to get bot response
+      this._chatService.sendMessage(userInput).subscribe(response => {
+        const botResponse = response.response;
+
+        // Add bot response to chatMessages
+        this.chatMessages.push({ content: botResponse, sender: 'bot' });
+      });
+    }
+  }
 }
