c435d1d30efd00e63640b9c576e07272bfb2648e
Raphael <matosramosraphael@gmail.com>
2024-05-27 11:18:14 -0500

    router deploy

--- src/app/shared/pages/partials/sidebar/sidebar.component.ts
+++ src/app/shared/pages/partials/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  constructor(
+    private router: Router
+  ) {
 
+  }
+  navigate(route: string) {
+    switch (route) {
+      case "user-home": {
+        this.router.navigate(['/user-home']);
+        break;
+      }
+      case "dashboard": {
+        this.router.navigate(['/dashboard']);
+        break;
+      }
+      case "alumnos": {
+        this.router.navigate(['/alumnos']);
+        break;
+      }
+    }
+  }
 }
