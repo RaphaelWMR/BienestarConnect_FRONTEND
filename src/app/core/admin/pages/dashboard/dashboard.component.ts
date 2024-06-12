8548bf038a3b49d412177fda842314022512fce6
RaphaelMR <matosramosraphael@gmail.com>
2024-06-11 20:18:00 -0500

    dashboard sin datos 2 pies

--- src/app/core/admin/pages/dashboard/dashboard.component.ts
+++ src/app/core/admin/pages/dashboard/dashboard.component.ts
@@ -50,10 +50,11 @@ export class DashboardComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.createPieChart();
+    this.createModalidadPieChart();
+    this.createTipoPieChart();
   }
 
-  createPieChart(): void {
+  createModalidadPieChart(): void {
     Chart.register(...registerables); // Registra todos los componentes necesarios
 
     const canvas = document.getElementById('modalidadPieChart') as HTMLCanvasElement;
@@ -65,7 +66,61 @@ export class DashboardComponent implements AfterViewInit {
           data: {
             labels: ["Presencial", "Virtual"],
             datasets: [{
-              data: [70, 30],
+              data: [6, 4],
+              backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+              hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+              hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
+          },
+          options: {
+            maintainAspectRatio: false,
+            plugins: {
+              tooltip: {
+                enabled: true, // Asegúrate de que los tooltips estén habilitados
+                backgroundColor: "rgb(255,255,255)",
+                bodyColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                padding: 15,
+                displayColors: true,
+                caretPadding: 10,
+                callbacks: {
+                  label: function(context) {
+                    let label = context.label || '';
+                    let value = context.raw || 0;
+                    return `${label}: ${value}`;
+                  }
+                }
+              },
+              legend: {
+                display: false
+              }
+            },
+            cutout: "80%",
+          },
+        });
+      } else {
+        console.error('Failed to get 2D context');
+      }
+    } else {
+      console.error('Canvas element not found');
+    }
+
+  }
+
+  createTipoPieChart(): void {
+    Chart.register(...registerables); // Registra todos los componentes necesarios
+
+    const canvas = document.getElementById('tipoPieChart') as HTMLCanvasElement;
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        new Chart(ctx, {
+          type: 'doughnut',
+          data: {
+            labels: ["Asesoría Piscopedagógica", "Orientación Académico - Administrativa"],
+            datasets: [{
+              data: [7, 3],
               backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
               hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
               hoverBorderColor: "rgba(234, 236, 244, 1)",
